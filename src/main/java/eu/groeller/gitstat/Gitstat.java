b6d97f68794ac2d9151f8b2ea31b14cf4d77d7ed
Luktronic <luktronic@gmx.at>
2025-03-20 11:32:13 +0100

    Added cli argument `--skip-chart` which skips chart generation
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/eu/groeller/gitstat/Gitstat.java
+++ src/main/java/eu/groeller/gitstat/Gitstat.java
@@ -29,12 +29,17 @@ public class Gitstat {
                 
                 System.out.println(resultTable.print());
 
+                if(args.length > 0 && args[0].equals("--skip-chart")) {
+                    System.out.println("\nSkipping chart generation");
+                } else {
+
                 List<DateCommitRecord> timeSeriesData = analyzer.getTimeSeriesData(true);
                 ChartGenerator chartGenerator = new ChartGenerator();
                 Path outputPath = Path.of("git-statistics.html");
-                
-                chartGenerator.generateCharts(timeSeriesData, outputPath);
-                System.out.println("\nCharts generated at: " + outputPath.toAbsolutePath());
+                    
+                    chartGenerator.generateCharts(timeSeriesData, outputPath);
+                    System.out.println("\nCharts generated at: " + outputPath.toAbsolutePath());
+                }
 
                 long endTime = System.currentTimeMillis();
                 System.out.printf("%nTime taken: %.2f seconds%n", (endTime - startTime) / 1000.0);
